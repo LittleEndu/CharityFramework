6002f5ec61c91aa3f4672a19a5bfd1c716727c41
LittleEndu <enduslittlecomputer@gmail.com>
2025-11-11 12:42:55 +0200

    Fix `domRightSidebar` patch

--- packages/core/src/patches/dom/index.ts
+++ packages/core/src/patches/dom/index.ts
@@ -4,10 +4,10 @@ export const domEvents = (): Patch[] => {
 	return [
 		{
 			name: 'domRightSidebar',
-			find: 'shop-profile-picture',
+			find: "You don't have charges to paint. Wait to recharge.",
 			replace: {
 				match:
-					/[a-zA-Z_$][\w$]*=[a-zA-Z_$][\w$]*=>{var ([a-zA-Z_$][\w$]*)=[a-zA-Z_$][\w$]*\(\)[^}]*?;(?=[^}]*?shop-profile-picture)/,
+					/[a-zA-Z_$][\w$]*=[a-zA-Z_$][\w$]*=>{var ([a-zA-Z_$][\w$]*)=[a-zA-Z_$][\w$]*\(\),[^}]*?;(?=[^}]*?__click=\[\$.*?"size-5")/,
 				replace: (orig, elem) =>
 					`${orig};document.dispatchEvent(new CustomEvent('charity-right-sidebar',{detail:${elem}}));`,
 			},
