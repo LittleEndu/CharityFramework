a6e09924683cab211ba828df3e30a5ae709399a3
LittleEndu <enduslittlecomputer@gmail.com>
2025-11-11 12:42:43 +0200

    Fix `trswStubSend` patch

--- packages/core/src/patches/trsw/index.ts
+++ packages/core/src/patches/trsw/index.ts
@@ -3,7 +3,7 @@ import { Patch } from '@placecharity/framework-types';
 export const trswPatches = (): Patch[] => [
 	{
 		name: 'trswStubSend',
-		find: "You're an using an older browser, some features might not work. Consider updating or changing browser.",
+		find: "Service Workers are not supported/enabled in your browser. Some features might not work properly.",
 		replace: {
 			match: /return new Promise\(\((.),.\)=>{.*?}catch\(.\){.\(.\)}}\)/,
 			replace: () => 'return Promise.resolve(null)',
